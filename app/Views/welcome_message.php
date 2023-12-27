84e13d71496f40dd7a080855eb4f23248f353ba8
Shubham Gupta <shubhamgupta7082@gmail.com>
2023-12-27 14:20:20 +0530

    update functionality

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -11,12 +11,15 @@
         </a>
     </div>
     <div class="card m-3" style="width: 18rem;">
-        <a href="<?= base_url('login') ?>" style="color: black; text-decoration: none;">
+    <!-- If admin login then it redirect to dashboard page otherwise it redirect to login page -->
+        <a href="<?php echo (session()->loggedin == 'loggedin') ? base_url('admin_dashboard') : base_url('login') ?>" style="color: black; text-decoration: none;">
             <div class="card-body">
                 <h3>Admin</h3>
             </div>
         </a>
+        
     </div>
+    <!-- <a href="<?php echo (session()->loggedin == 'loggedin') ? base_url('admin_dashboard') : base_url('login') ?>">hello</a> -->
 </div>
 
 <?= $this->endSection() ?>
